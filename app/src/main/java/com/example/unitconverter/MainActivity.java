9af9f4420155081edc2b482b5f9654d6a3b119f7
nguetcheu <dnguetcheu@gmail.com>
2024-11-20 15:34:30 +0100

    Ajout des id et definition de la logique

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.unitconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText myEditText = findViewById(R.id.myEditText);
+        TextView myTextView = findViewById(R.id.myTextView);
+        Button myButton = findViewById(R.id.myButton);
+
+        myTextView.setText("0");
+
+        View.OnClickListener listerButton = new View.OnClickListener() {
+            @Override
+            public void onClick(View buttonView) {
+                Button button = (Button) buttonView;
+                String weight = myEditText.getText().toString();
+                myTextView.setText(String.valueOf(weightToKg(weight)));
+            }
+        };
+
+        myButton.setOnClickListener();
     }
+
+    Double weightToKg(String weight){
+        double result = Double.parseDouble(weight);
+        // 1kg = 2.2 Ibs
+        if (result == 0.0){
+            return 0.0;
+        }
+        result *= 2.2;
+        return result;
+    };
 }
